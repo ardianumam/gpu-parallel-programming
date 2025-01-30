a9a09827824719a3138bfc97fac226b59112f76c
Ardian Umam <ardianumam@gmail.com>
2025-01-30 12:08:40 +0800

    vectorize 2D block tiling matmul kernel

--- 07_Faster_Matmul/SGEMM_CUDA/src/kernels/6_kernel_vectorize.cuh
+++ 07_Faster_Matmul/SGEMM_CUDA/src/kernels/6_kernel_vectorize.cuh
@@ -12,16 +12,27 @@
 template <const int BM, const int BN, const int BK, const int TM, const int TN>
 __global__ void sgemmVectorize(int M, int N, int K, float alpha, float *A,
                                float *B, float beta, float *C) {
+  /*
+  Important note: 
+  The driver program (runner.cu) provide two different blockDim. This kernel only valid for the first dim, with hyoerparameters specified below. 
+    const uint BK = 8;
+    const uint TM = 8;
+    const uint TN = 8;
+    const uint BM = 128;
+    const uint BN = 128;
+    dim3 gridDim(CEIL_DIV(N, BN), CEIL_DIV(M, BM));
+    dim3 blockDim((BM * BN) / (TM * TN)) = (256);
+  */
   const uint cRow = blockIdx.y;
   const uint cCol = blockIdx.x;
 
   // BN/TN are the number of threads to span a column
-  const int threadCol = threadIdx.x % (BN / TN);
-  const int threadRow = threadIdx.x / (BN / TN);
+  const int threadCol = threadIdx.x % (BN / TN); // range = [0, 15]
+  const int threadRow = threadIdx.x / (BN / TN); // range = [0, 15]
 
   // allocate space for the current blocktile in smem
-  __shared__ float As[BM * BK];
-  __shared__ float Bs[BK * BN];
+  __shared__ float As[BM * BK]; // size = [1024]
+  __shared__ float Bs[BK * BN]; // size = [1024]
 
   // Move blocktile to beginning of A's row and B's column
   A += cRow * BM * K;
@@ -29,11 +40,11 @@ __global__ void sgemmVectorize(int M, int N, int K, float alpha, float *A,
   C += cRow * BM * N + cCol * BN;
 
   // calculating the indices that this thread will load into SMEM
-  // we'll load 128bit / 32bit = 4 elements per thread at each step
-  const uint innerRowA = threadIdx.x / (BK / 4);
-  const uint innerColA = threadIdx.x % (BK / 4);
-  const uint innerRowB = threadIdx.x / (BN / 4);
-  const uint innerColB = threadIdx.x % (BN / 4);
+  // we'll load 128bit / bit_per_float = 128bit / 32bit = 4 elements per thread at each step
+  const uint innerRowA = threadIdx.x / (BK / 4); // 256 / (8/4) -> range = [0, 127]
+  const uint innerColA = threadIdx.x % (BK / 4); // 256 % (8/4) -> range = [0, 1]
+  const uint innerRowB = threadIdx.x / (BN / 4); // 256 / (128/4) -> range = [0, 7]
+  const uint innerColB = threadIdx.x % (BN / 4); // 256 % (128/4) -> range = [0, 31]
 
   // allocate thread-local cache for results in registerfile
   float threadResults[TM * TN] = {0.0};
@@ -42,25 +53,28 @@ __global__ void sgemmVectorize(int M, int N, int K, float alpha, float *A,
 
   // outer-most loop over block tiles
   for (uint bkIdx = 0; bkIdx < K; bkIdx += BK) {
-    // populate the SMEM caches
-    // transpose A while loading it
+    // load chunk data from GMEM to SMEM. Let's start from B to Bs that is the easier one
+    reinterpret_cast<float4 *>(&Bs[innerRowB * BN + innerColB * 4])[0] =
+    reinterpret_cast<float4 *>(&B[innerRowB * N + innerColB * 4])[0];
+    // Bs = BK x BN = 8 x 128. innerRowB ranges [0, 7], populating the BK dimension. innerColB ranges [0, 31], each represent vector of 4 floats, thus representing 32x4 = 128 elements to populate BN dimension of Bs
+    
+    // to load the A to As, it is similar to "B to BS", with additional of transpose operation to enable coalescing in regM by aligning it in column-major order
     float4 tmp =
-        reinterpret_cast<float4 *>(&A[innerRowA * K + innerColA * 4])[0];
+        reinterpret_cast<float4 *>(&A[innerRowA * K + innerColA * 4])[0]; // As = BM x BK = 128 x 8. innerRowB ranges [0, 127], populating the BM dimension. innerColB ranges [0, 1], each represent vector of 4 floats, thus representing 2x4 = 8 elements to populate BK dimension of Bs
+    // below reassignment operation by modifying the index for transpose operation
     As[(innerColA * 4 + 0) * BM + innerRowA] = tmp.x;
     As[(innerColA * 4 + 1) * BM + innerRowA] = tmp.y;
     As[(innerColA * 4 + 2) * BM + innerRowA] = tmp.z;
     As[(innerColA * 4 + 3) * BM + innerRowA] = tmp.w;
 
-    reinterpret_cast<float4 *>(&Bs[innerRowB * BN + innerColB * 4])[0] =
-        reinterpret_cast<float4 *>(&B[innerRowB * N + innerColB * 4])[0];
     __syncthreads();
 
     // advance blocktile
     A += BK;     // move BK columns to right
     B += BK * N; // move BK rows down
 
-    // calculate per-thread results
-    for (uint dotIdx = 0; dotIdx < BK; ++dotIdx) {
+    // calculate per-thread results 
+    for (uint dotIdx = 0; dotIdx < BK; ++dotIdx) { // 
       // block into registers
       for (uint i = 0; i < TM; ++i) {
         regM[i] = As[dotIdx * BM + threadRow * TM + i];
